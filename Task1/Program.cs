479bb19466033f212b84657bbf7c5abb8d9b0249
Armen <aapapikyan@gmail.com>
2023-02-12 11:14:43 +0300

    Задания к семинару 4 выполнены

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Promt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int Exp(int num, int power)
+{
+    int result = num;
+    for(int i = 1; i < power; i++)
+    {
+        result = num*result;
+    }
+    return result;
+}
+
+int number = Promt("Введите число");
+int power = Promt("Введите натуральную степень");
+if(power > 0)
+{
+System.Console.WriteLine($"Число {number} в степени {power} равняется {Exp(number, power)}");
+}
+else
+{
+    System.Console.WriteLine("Степень должна быть больше ноля");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Promt(string message)
+{
+    System.Console.Write($"{message} > ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int Digits(int input)
+{
+int result = 1;
+int delitel = 10;
+int count = 0;    
+while (result > 0)
+{
+    result = input / delitel;
+    count++;
+    delitel = delitel*10;
+}
+return count;
+}
+
+int number = Promt("Введите число");
+int rest = 0;
+int delitel = 10;
+int sum = 0;
+for(int i = 0; i < Digits(number); i++)
+{
+    rest = number % delitel;
+    rest = rest / (delitel / 10);
+    delitel = delitel*10;
+    sum = sum + rest;
+}
+System.Console.WriteLine($"Сумма цифр числа {number} равняется {sum}");
